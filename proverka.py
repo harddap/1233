817bb5a23a75dad8507e1f2f054a0a4ef3dd2206
harddap <144228835+harddap@users.noreply.github.com>
2023-10-09 10:07:38 +0300

    Add files via upload

---
+++ proverka.py
@@ -0,0 +1,48 @@
+#num = int(input())
+#x = 0
+#for i in range(2, num // 2+1):
+    #if(num % i == 0):
+        #x = x+1
+#if (x <= 0):
+   #print("простое")
+#else:
+    #print("не простое")
+
+
+#n = 1
+#n1 = 1
+#x = (input()
+#x = int(x)
+#i = 0
+#while i < x - 2:
+    #t = n + n1
+    #t1 = t1
+    #t2 = t
+    #i = i + 1
+#print(n1)
+
+
+#n = 10
+#x = []
+#for i in range(n):
+    #x.append(randint(1, 99))
+#print(x)
+#for i in range(n - 1):
+    #for j in range(n - i - 1):
+        #if x[j] > x[j + 1]:
+           # x[j], x[j + 1] = x[j + 1], x[j]
+    #print(x)
+
+
+
+#n=4
+#num = [randint(1, 20) for i in range(n)]
+#print (num)
+#max = num[0]
+#min = num[0]
+#for i in range(N):
+    #if num[i] > Max : Max=num[i]
+    #if num[i] < Min : Min=num[i]
+#print ( Max, Min)
+
+
